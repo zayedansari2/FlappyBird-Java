9b98673c83017730e2826e2cb0004a707d4bbd6f
zayedansari2 <zayedansari112@gmail.com>
2024-07-01 19:36:35 -0700

    Create FlappyBirdMain.java

---
+++ src/FlappyBirdMain.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class FlappyBirdMain {
+
+    //Short main method to run the game
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        FlappyBirdGame game = new FlappyBirdGame();
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
